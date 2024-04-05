688482058832a54a646014d2ddaf40882ba42ffc
Joao Labussiere <labussework@gmail.com>
2024-04-05 03:16:02 -0300

    Indentação

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,17 @@
 import { NavLink } from "./nav-link";
 
 export function Header() {
-    return (
-        <div className="flex items-center gap-5 py-2">
+  return (
+    <div className="flex items-center gap-5 py-2">
+      <img src="src\image\nlwImage.svg" alt="Logo NLW" />
 
-            <img src="src\image\nlwImage.svg" alt="Logo NLW" />
-
-            <nav className="flex items-center gap-5">
-                <NavLink title="events" href="/eventos">Eventos</NavLink>  {/* className="font-medium text-sm text-zinc-300" */}
-                <NavLink href="/participantes">Participantes</NavLink>
-            </nav>
-        </div>
-    )
+      <nav className="flex items-center gap-5">
+        <NavLink title="events" href="/eventos">
+          Eventos
+        </NavLink>{" "}
+        
+        <NavLink href="/participantes">Participantes</NavLink>
+      </nav>
+    </div>
+  );
 }
