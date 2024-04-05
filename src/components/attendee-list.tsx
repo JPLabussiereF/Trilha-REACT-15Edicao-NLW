a9bd7e555ad8e1dcea61dc9bc7760669340426c6
Joao Labussiere <labussework@gmail.com>
2024-04-05 02:15:50 -0300

    NLW Unite | Aula 03/Final

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -7,40 +7,106 @@ import { Table } from "./table/table"
 import { TableHeader } from "./table/table-header"
 import { TableCell } from "./table/table-cell"
 import { TableRow } from "./table/table-row"
-import { ChangeEvent, useState } from "react"
-import { attendees } from "../data/attendees"
+import { ChangeEvent, useEffect, useState } from "react"
+
 
 dayjs.extend(relativeTime)
 dayjs.locale("pt-br")
 
+interface Attendee {
+    id:string
+    name:string
+    email:string
+    createdAt:string
+    checkedInAt: string | null
+}
+
 export function AttendeeList(){
-    const [search, setSearch] = useState("")
-    const [page, setPage] = useState(1)
+    const [search, setSearch] = useState(() => {
+        const url = new URL(window.location.toString())
 
-    const totalPages = Math.ceil(attendees.length / 10)
+        if (url.searchParams.has("search")){
+            return url.searchParams.get("search") ?? ""
+        }
 
-    function OnSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-        setSearch(event.target.value)
+        return ""
+    })
+
+
+
+    const [page, setPage] = useState(() => {
+        const url = new URL(window.location.toString())
+
+        if (url.searchParams.has("page")){
+            return Number(url.searchParams.get("page"))
+        }
+
+        return 1
+    })
+
+    const [total, setTotal] = useState(0)
+    const [attendees, setAttendees] = useState<Attendee[]>([])
+    
+    const totalPages = Math.ceil(total / 10)
+
+    useEffect(() => {
+        const url = new URL("http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees")
+
+        url.searchParams.set("pageIndex", String(page - 1))
+
+        if(search.length > 0){
+        url.searchParams.set("query", search)
+        }
+
+        fetch(url)
+            .then(response => response.json())
+                .then(data => {
+                    setAttendees(data.attendees)
+                    setTotal(data.total)
+
+                })
+        }, [page, search])
+
+    function setCurrentSearch(search: string){
+        const url = new URL(window.location.toString())
+
+        url.searchParams.set("search", search)
+
+        window.history.pushState({}, "", url)
+
+        setSearch(search)
     }
 
+    function setCurrentPage(page: number){
+        const url = new URL(window.location.toString())
+
+        url.searchParams.set("page", String(page))
+
+        window.history.pushState({}, "", url)
+
+        setPage(page)
+    }
+
+    function OnSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
+        setCurrentSearch(event.target.value)
+        setCurrentPage(1)
+    }
+    
     function goToPreviousPage(){
-        setPage(page - 1)
+        setCurrentPage(page - 1)
     }
     
     function goToFirstPage(){
-        setPage(1)
+        setCurrentPage(1)
     }
 
     function goToLastPage(){
-        setPage(totalPages)
+        setCurrentPage(totalPages)
     }
 
     function goToNextPage(){
-        setPage(page + 1)
+        setCurrentPage(page + 1)
     }
-
-    
-
 
     return (
         <div className="flex flex-col gap-4">
@@ -48,9 +114,12 @@ export function AttendeeList(){
                 <h1 className="text-2xl font-bold">Participantes</h1>
             <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
                 <img src="src\image\minilupa.svg" alt="Mini icone de lupa" />
-                <input onChange={OnSearchInputChanged} className="bg-transparent flex-1 outline-none h-auto border-0 p-0 text-sm" placeholder="Buscar participante..." type="text" />
+                <input 
+                    onChange={OnSearchInputChanged} 
+                    value={search}
+                    className="bg-transparent flex-1 outline-none h-auto border-0 p-0 text-sm focus:ring-0" 
+                    placeholder="Buscar participante..."  />
                 </div>
-                {search}
             </div>
 
                 <Table>
@@ -66,7 +135,7 @@ export function AttendeeList(){
                         </tr>
                     </thead>    
                     <tbody>
-                        {attendees.slice((page - 1) * 10, page * 10 ).map((attendee) => {
+                        {attendees.map((attendee) => {
                             return (
                                 <TableRow key={attendee.id}>
                             <TableCell>
@@ -79,7 +148,11 @@ export function AttendeeList(){
                                 </div>
                             </TableCell>
                             <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
-                            <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
+                            <TableCell>
+                                {attendee.checkedInAt === null 
+                                ? <span className="text-zinc-400">Não fez check-in</span> 
+                                : dayjs().to(attendee.checkedInAt)}
+                            </TableCell>
                             <TableCell>
                                 <IconButton transparent>
                                     <MoreHorizontalIcon className="size-4"></MoreHorizontalIcon>
@@ -92,7 +165,7 @@ export function AttendeeList(){
                     <tfoot>
                         <tr>
                             <TableCell className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
-                                Mostrando 10 de {attendees.length} itens
+                                Mostrando {attendees.length} de {total} itens
                             </TableCell>
                             <TableCell className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
                                 <div className="inline-flex items-center gap-8">
@@ -118,5 +191,4 @@ export function AttendeeList(){
                 </Table>
         </div>
     )
-
 }
