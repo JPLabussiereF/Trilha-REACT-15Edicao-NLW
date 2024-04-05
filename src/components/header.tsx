7446cfa6190ae5813eb9fa4669aed5fafdbeb1ba
Joao Labussiere <labussework@gmail.com>
2024-04-04 22:03:08 -0300

    NLW Unite | Aula 02

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "./nav-link";
+
 export function Header() {
     return (
         <div className="flex items-center gap-5 py-2">
@@ -5,8 +7,8 @@ export function Header() {
             <img src="src\image\nlwImage.svg" alt="Logo NLW" />
 
             <nav className="flex items-center gap-5">
-                <a href="" className="font-medium text-sm text-zinc-300">Eventos</a>
-                <a href="" className="font-medium text-sm">Participantes</a>
+                <NavLink title="events" href="/eventos">Eventos</NavLink>  {/* className="font-medium text-sm text-zinc-300" */}
+                <NavLink href="/participantes">Participantes</NavLink>
             </nav>
         </div>
     )
